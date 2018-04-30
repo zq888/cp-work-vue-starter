740d8e00a3aed1faa30fcf2e1d161d0a0d266ac4
linuxing3 <linuxing3@qq.com>
2018-04-29 20:25:38 -0400

    Delete lambda

--- src/lambda/hello.js
+++
@@ -1,9 +0,0 @@
-import { log } from "@/util";
-
-export function handler(event, context, callback) {
-  log.info(event);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: "Hello, World!" })
-  });
-}
