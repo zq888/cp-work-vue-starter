b6eb8cef54f5ed0bd166384741e2e7724c5bebc2
linuxing3 <linuxing3@qq.com>
2018-05-02 07:02:38 -0400

    fb as

--- src/components/User/UserLogin.vue
+++ src/components/User/UserLogin.vue
@@ -30,89 +30,84 @@
   </form>
 </template>
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import { State, Action, Getter } from "vuex-class";
+import { Component, Prop, Vue } from "vue-property-decorator"
+import { State, Action, Getter } from "vuex-class"
 
-const Netlify = require("netlify-auth-providers");
+const Netlify = require("netlify-auth-providers")
 
-import fb from "@/store/api/firebaseSDK";
+import { fb } from "@/store/api/firebaseSDK"
 
-import * as types from "@/store/types";
+import * as types from "@/store/types"
 
 @Component
 export default class UserLogin extends Vue {
-  name: string;
-  email: string;
-  password: string;
-  code: string;
-  select: any;
-  dictionary: any;
+  name: string
+  email: string
+  password: string
+  code: string
+  select: any
+  dictionary: any
   @State("name", { namespace: "User" })
-  userDbName!: string;
+  userDbName!: string
   @State("items", { namespace: "User" })
-  items: any[];
+  items: any[]
   @Action(types.aCreate, { namespace: "User" })
-  createUser: Function;
+  createUser: Function
 
   constructor() {
-    super();
-    this.name = "linuxing3";
-    this.email = "linuxing3@qq.com";
-    this.password = "20090909";
-    this.code = "";
+    super()
+    this.name = "linuxing3"
+    this.email = "linuxing3@qq.com"
+    this.password = "20090909"
+    this.code = ""
     this.dictionary = {
       attributes: {
-        email: "E-mail Address"
+        email: "E-mail Address",
       },
       custom: {
         name: {
           required: () => "Name can not be empty",
-          max: "The name field may not be greater than 10 characters"
+          max: "The name field may not be greater than 10 characters",
           // custom messages
         },
         password: {
-          required: "Select field is required"
-        }
-      }
-    };
+          required: "Select field is required",
+        },
+      },
+    }
   }
   mounted() {}
 
   netlifyLogin() {
     // this.createUser({ name: this.name, email: this.email });
-    var authenticator = new Netlify.default({});
-    authenticator.authenticate(
-      { provider: "github", scope: "user" },
-      (err: any, data: any) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log("Authenticated with GitHub. Access Token: " + data.token);
+    var authenticator = new Netlify.default({})
+    authenticator.authenticate({ provider: "github", scope: "user" }, (err: any, data: any) => {
+      if (err) {
+        console.log(err)
       }
-    );
+      console.log("Authenticated with GitHub. Access Token: " + data.token)
+    })
   }
 
   async fireSignup() {
-    fb.firebaseAuth
-      .createUserWithEmailAndPassword("linuxing3@qq.com", "20090909")
-      .catch(data => {
-        console.log(data);
-      });
+    fb.firebaseAuth.createUserWithEmailAndPassword("linuxing3@qq.com", "20090909").catch(data => {
+      console.log(data)
+    })
   }
 
   async fireSignin() {
-    let user = await fb.loginWithEmail(this.email, this.password);
-    console.log(user);
+    let user = await fb.loginWithEmail(this.email, this.password)
+    console.log(user)
   }
 
   doneLogin() {
-    this.$router.push({ path: "/home" });
+    this.$router.push({ path: "/home" })
   }
 
   clearFields() {
-    this.name = "";
-    this.email = "";
-    this.select = null;
+    this.name = ""
+    this.email = ""
+    this.select = null
   }
 }
 </script>
