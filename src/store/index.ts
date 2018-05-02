357ae7ce29b23da600d0bdadf3b65a08cdb9edea
linuxing3 <linuxing3@qq.com>
2018-05-02 08:24:35 -0400

    dbInit error in store/index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,36 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from "vue"
+import Vuex from "vuex"
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-import fb from "@/store/api/firebaseSDK";
-fb.dbInit();
-Vue.prototype.$fb = fb;
+import { fb } from "@/store/api/firebaseSDK"
+fb.dbInit()
 
-import { dbInit } from "@/store/api/NedbSDK";
-dbInit();
-// import nb from "@/store/api/NedbSDK";
-// nb.dbInit();
-// Vue.prototype.$nb = nb;
+import { dbInit } from "@/store/api/NedbSDK"
+dbInit()
 
-import User from "./modules/User";
-import Database from "./modules/Database";
-import Document from "./modules/Document";
-import plugins from "./plugin";
+import User from "./modules/User"
+import Database from "./modules/Database"
+import Document from "./modules/Document"
+import plugins from "./plugin"
 
-import Base from "@/store/modules/Base";
+import Base from "@/store/modules/Base"
 
 interface IState {
-  title: string;
-  filterKey: string;
-  token: string;
+  title: string
+  filterKey: string
+  token: string
 }
 
 const state: IState = {
   title: "xing wenju",
   filterKey: "",
-  token: ""
-};
+  token: "",
+}
 
 export default new Vuex.Store({
   state,
@@ -38,11 +34,11 @@ export default new Vuex.Store({
   modules: {
     User,
     Database,
-    Document
+    Document,
   },
   getters: {
-    mytitle: state => state.title
+    mytitle: state => state.title,
   },
   mutations: Base.mutations,
-  actions: Base.actions
-});
+  actions: Base.actions,
+})
